0b9c606e5a688961d1be1df9238b85190c3f5a2e
king-ak <akdavey@email.wm.edu>
2023-06-02 18:31:27 -0400

    Add logic to configure AAD access + create AKV backed secret scope

--- scripts/configure_databricks_workspace.sh
+++ scripts/configure_databricks_workspace.sh
@@ -1,4 +1,6 @@
-# Install Databricks CLI
+WORKSPACE_NAME=$1
+KEYVAULT_NAME=$2
+
 
 # Set up venv
 rm -rf ./venv
@@ -14,8 +16,22 @@ echo "Installing requirements from ${requirements_file}..."
 pip install -r ${requirements_file}
 echo "Installed requirements from ${requirements_file}..."
 
+# Fetch AAD token scoped for Databricks use
+export DATABRICKS_AAD_TOKEN=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d | jq -r .accessToken)
+
+# Collect Resource Information for Workspace and Keyvault
+DATABRICKS_URL=$(az databricks workspace show --name $WORKSPACE_NAME --resource-group $RG_NAME | jq -r .workspaceUrl)
+KV_RESOURCE_ID=$(az keyvault show --name $KEYVAULT_NAME --resource-group $RG_NAME | jq -r .id)
+KV_DNS_NAME=$(az keyvault show --name $KEYVAULT_NAME --resource-group $RG_NAME | jq -r .properties.vaultUri)
 
 
 
+# Configure Databricks CLI using AAD token
+databricks configure --aad-token --host "https://$DATABRICKS_URL"
 
-
+# Create Databricks Secret Scope for KV
+databricks secrets create-scope --scope-backend-type AZURE_KEYVAULT \
+                                --resource-id $KV_RESOURCE_ID \
+                                --dns-name $KV_DNS_NAME \
+                                --scope $KEYVAULT_NAME \
+                                --initial-manage-principal users
