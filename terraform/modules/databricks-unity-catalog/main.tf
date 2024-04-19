f23626bee9eb66b642d297aa20dd1c5470698d88
king-ak <akdavey@email.wm.edu>
2024-04-18 20:30:39 -0400

    Check in TF changes

--- terraform/modules/databricks-unity-catalog/main.tf
+++ terraform/modules/databricks-unity-catalog/main.tf
@@ -14,17 +14,17 @@ resource "databricks_sql_endpoint" "general_small_endpoint" {
 }
 
 resource "databricks_metastore" "this" {
-  name          = "dbxpocms"
-  region        = var.databricks_location
-  storage_root  = var.databricks_storage_root
-  delta_sharing_scope = "INTERNAL"
+  name                                              = "dbxpocms"
+  region                                            = var.databricks_location
+  storage_root                                      = var.databricks_storage_root
+  delta_sharing_scope                               = "INTERNAL"
   delta_sharing_recipient_token_lifetime_in_seconds = 3600
 }
 
 resource "databricks_metastore_assignment" "this" {
-  workspace_id         = var.databricks_workspace_id
-  metastore_id         = databricks_metastore.this.id
-  depends_on = [databricks_metastore.this]
+  workspace_id = var.databricks_workspace_id
+  metastore_id = databricks_metastore.this.id
+  depends_on   = [databricks_metastore.this]
 }
 
 resource "databricks_storage_credential" "external" {
@@ -32,6 +32,6 @@ resource "databricks_storage_credential" "external" {
   azure_managed_identity {
     access_connector_id = var.databricks_external_access_connector_id
   }
-  comment = "Managed by Terraform"
+  comment    = "Managed by Terraform"
   depends_on = [databricks_metastore_assignment.this]
 }
